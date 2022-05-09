38d0a44ee7b79e7d5a75c4dfbc500f95c2e9fdda
alicialph <46506346+alicialph@users.noreply.github.com>
2022-05-09 17:29:33 +1000

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -7,7 +7,9 @@ class minecraft {
     source => 'https://launcher.mojang.com/v1/objects/c8f83c5655308435b3dcf03c06d9fe8740a77469/server.jar',
   }
   package {'java':
+	  provider => 'rpm',
     ensure => present,
+	  source => 'https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.rpm',
   }
   file {'/opt/minecraft/eula.txt':
     ensure => file,

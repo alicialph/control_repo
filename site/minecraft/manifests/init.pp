c1b8979c98eb1360939646379869347be3cdd9c6
alicialph <46506346+alicialph@users.noreply.github.com>
2022-05-09 17:52:27 +1000

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -6,28 +6,23 @@ class minecraft {
   	file {'/opt/minecraft':
     		ensure => directory,
   	}
- 
  	file {"${install_dir}/minecraft_server.jar":
     		ensure => file,
     		source => $url,
 	}
-	  
   	package {'java':
 		provider => 'rpm',
     		ensure => present,
 		source => $java_url,
   	}
-	
   	file {"${install_dir}/eula.txt":
     		ensure => file,
     		content => 'eula=true',
   	}
-	
   	file {'/etc/systemd/system/minecraft.service':
     		ensure => file,
     		source => 'puppet:///modules/minecraft/minecraft.service',
   	}
-	
   	service {'minecraft':
 		ensure => running,
 		enable => true,

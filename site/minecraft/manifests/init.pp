1d9317996445b740245183709c90924aab584da2
alicialph <46506346+alicialph@users.noreply.github.com>
2022-05-09 17:52:08 +1000

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,22 +1,35 @@
 class minecraft {
-  file {'/opt/minecraft':
-    ensure => directory,
-  }
-  file {'/opt/minecraft/minecraft_server.jar':
-    ensure => file,
-    source => 'https://launcher.mojang.com/v1/objects/c8f83c5655308435b3dcf03c06d9fe8740a77469/server.jar',
-  }
-  package {'java':
-	  provider => 'rpm',
-    ensure => present,
-	  source => 'https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.rpm',
-  }
-  file {'/opt/minecraft/eula.txt':
-    ensure => file,
-    content => 'eula=true',
-  }
-  file {'/etc/systemd/system/minecraft.service':
-    ensure => file,
-    source => 'puppet:///modules/minecraft/minecraft.service',
-  }
+	$url = 'https://launcher.mojang.com/v1/objects/c8f83c5655308435b3dcf03c06d9fe8740a77469/server.jar'
+	$java_url = 'https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.rpm'
+  	$install_dir = '/opt/minecraft'
+  
+  	file {'/opt/minecraft':
+    		ensure => directory,
+  	}
+ 
+ 	file {"${install_dir}/minecraft_server.jar":
+    		ensure => file,
+    		source => $url,
+	}
+	  
+  	package {'java':
+		provider => 'rpm',
+    		ensure => present,
+		source => $java_url,
+  	}
+	
+  	file {"${install_dir}/eula.txt":
+    		ensure => file,
+    		content => 'eula=true',
+  	}
+	
+  	file {'/etc/systemd/system/minecraft.service':
+    		ensure => file,
+    		source => 'puppet:///modules/minecraft/minecraft.service',
+  	}
+	
+  	service {'minecraft':
+		ensure => running,
+		enable => true,
+  	}
 }
